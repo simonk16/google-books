ac980aa9c9b12226c1dac9aa335ddc5fa481bc14
Kraus, Simon <simon.kraus@aol.com>
2019-09-17 15:26:15 -0500

    mLab

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose")
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,14 +12,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:Password1@ds229648.mlab.com:29648/heroku_ssnczpxx", { useNewUrlParser: true })
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
